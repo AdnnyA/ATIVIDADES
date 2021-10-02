bb51f1e3ee25123d6964765b60dd485abd30f4e2
AdnnyA <eeep.adnaellealves@gmail.com>
2021-10-02 11:46:07 -0300

    atv 01

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Cabecalho from './src/cabecalho/Cabecalho'
+import Corpo from './src/corpo/Corpo'
+import Disciplina from './src/disciplina/Disciplina'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Cabecalho
+        nome = 'Adnny'
+        curso = 'Design Digital' />
+      <Corpo />
+      <Disciplina
+        disciplina = 'PIDM' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
